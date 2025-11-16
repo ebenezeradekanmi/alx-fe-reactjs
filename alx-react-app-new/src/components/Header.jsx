b3fda94f3af3378ad56bfd4b148151373eae7b8c
ebenezeradekanmi <ebenezeradekanmi2018@gmail.com>
2025-11-16 14:18:33 +0100

    Add components with inline styling and App.jsx

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,7 +1,17 @@
-export default function Header() {
+import React from "react";
+
+const Header = () => {
   return (
-    <header style={{ backgroundColor: 'blue', color: 'white', textAlign: 'center', padding: '1rem' }}>
-      <h1>My App Header</h1>
+    <header
+      style={{
+        backgroundColor: "#282c34",
+        color: "white",
+        textAlign: "center",
+      }}
+    >
+      <h1>Welcome to My App</h1>
     </header>
   );
-}
+};
+
+export default Header;
